4d414be2f814a8bc42150a7c5fc2535601af7896
Hayley Dixon <hdixon37@gmail.com>
2021-08-08 09:53:32 -0600

    Add Styling

---
+++ src/apiCalls.js
@@ -0,0 +1,19 @@
+// export const destinationData = fetch("http://localhost:3001/api/v1/destinations")
+//   .then(response => response.json())
+//   .then(data => data)
+//   .catch(err => console.log(err))
+// };
+// // export const tripData = fetch("http://localhost:3001/api/v1/trips")
+// //   .then(response => response.json())
+// //   .then(data => data)
+// //   .catch(err => console.log(err))
+// // }
+// export const travelerData = fetch("http://localhost:3001/api/v1/travelers")z
+//   .then(response => response.json())
+//   .then(data => data)
+//   .catch(err => console.log(err))
+// }
+//
+// export const allData = Promise.all((([destinationData, tripData, travelerData])))
+//   .then(values => parseValues(values))
+//   .catch(err => console.log('could not retrieve data'))
--- src/scripts.js
+++ src/scripts.js
@@ -4,7 +4,24 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
+import Traveler from './Traveler';
+import Trip from './Trip';
+import Destination from './Destination';
+
+import {allData, destinationData, tripData, travelerData} from './apiCalls';
+
+
+
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+// const fetchData = () => {
+//   addData(trip)
+// }
+const invokeFetch = () => {
+  allData.then(data => {
+    console.log(data)
+  })
+  .catch(err => console.log(err))
+}
